718bbd0287707a3175be58684240eeaa8e7b2940
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-08-09 21:48:19 -0400

    while loop practice

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,14 @@
 <h1><?php bloginfo('name')?></h1>
+
+<?php
+
+$names = array("eric", "kerry", "missy");
+
+$count = 0;
+
+while($count < count($names)){
+    echo "<li> Hi, my name is $names[$count]</li>";
+    $count++;
+}
+
+?>
