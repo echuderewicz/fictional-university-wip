22074f2e45c58db49db7de4911fbce4aa726d752
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-08-09 22:46:55 -0400

    trivial test commit

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -4,7 +4,7 @@
 while(have_posts()){
     the_post();?>
 
-    <h1>This is a page not a post</h1>
+    <h1>This is a page not a post OK</h1>
 
     <h2><?php the_title();?></h2>
     <?php the_content(); ?>
